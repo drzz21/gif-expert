9afa8fb5e44b40ee3b527c45631fcca6a48442c2
Francisco D <drz.y2k@gmail.com>
2022-08-24 14:29:55 -0500

    75. Agregar una nueva categoría
    para modificar un estado, debemos hacer una copia del estado y agregar el nuevo valor, en caso de que sea por ejemplo un arreglo, solo podemos hacer la mutacion con el set del hook, de otro modo no va a funcionar,
    las formas recomendadas son agregando la destrucutracion par aobtener todos los datos anteriores y agregar el nuevo, o tambien podemos usar el callback que se puede mandar al set, donde le mdanamos de igual forma el parametro de categorias con el alias que queramos, y le mandamos el nuevo valor

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,10 +2,16 @@ import { useState } from 'react';
 
 export const GifExpertApp = () => {
 	const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+	const onAddCategory = () => {
+		setCategories(['Valorant', ...categories]);
+		// setCategories([...categories,'Valorant']);
+		//setCategories((cat) => [...cat, 'valorant']);
+	};
 
 	return (
 		<>
 			<h1>GifExpert</h1>
+			<button onClick={onAddCategory}>Agregar</button>
 			<ol>
 				{categories.map((category) => {
 					return <li key={category}>{category}</li>;
