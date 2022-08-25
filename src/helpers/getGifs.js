732622b978d850158bc8b7c4f9079c153f0ae212
Francisco D <drz.y2k@gmail.com>
2022-08-25 13:21:06 -0500

    81. Fetch API - Obtener las imágenes deseadas
    no debería haber llamadas a funcioines dentro de functional components, esto es porque cada que se actualice el componente se hace de nuevo la ejecucion de funciones, lo cual nos haria duplicar el contenido
    
    modificamos el gifgrid, llamamos una funcion que se encarga de importar los gifs, esa la podemos traer en otro archivo e importarla desde el componente
    
    esa funcion lo que hace es hacer un fetch ala rul de la api de giphy, parsear el serializable stream que retorna el fetch usando el metodo json, y finalmente lo que hacemos es de los datos que obtenemos, destructurar la data que es lo que retorna en sí la peticion http, y de esa data por cada elemento vamos a retornar un nuevo objeto que será lo que retornaremos en el metodo de gifs

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,21 @@
+export const getGifs = async (category) => {
+  const url = `http://api.giphy.com/v1/gifs/search?api_key=U4P1GKXEB1Y38CeZLvTCRlNryZY9HC7I&q=${category}&limit=20`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  // const gifs=data.map(img=>{
+  //   return {
+
+  //   }
+  // })
+
+  //se puede reducir a
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+
+  console.log(gifs);
+  return gifs;
+};
