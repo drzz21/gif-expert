5646e27a1d7898396ad1ebb5781468c688e93e86
Francisco D <drz.y2k@gmail.com>
2022-08-25 16:07:30 -0500

    84. Mostrar los títulos de las imágenes
    useeffect no puede ser asincrono, porque al ahcer algo asincrono implicitamente retornamos una promesa y useeffect no puede retornar proremsas solo puede retornar una funcion y es para cleanup esa funcion que puede retornar

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,34 @@
-import { useEffect } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
+	const [images, setImages] = useState([]);
+
+	const getImages = async () => {
+		const newImages = await getGifs(category);
+		setImages(newImages);
+	};
+
 	//si se deja la dependencia vacia de este hook//
 	//esto le dice a react que esto solo se va a ejecutar la primera vez que se ejecuta el componente
 	useEffect(() => {
-		getGifs(category);
+		//no podemos uasr await aquí, podemos usar el then, ya que intermanete getgifs es un metodo promesa
+		// getGifs(category).then((newImages) => setImages(newImages));
+
+		//otra forma es como la pusimos ahora, ponemos el metodo async fuera del useeffect y de aqui lo llamamos
+		getImages();
 	}, []);
 
 	return (
 		<>
 			<h3>{category}</h3>
+			<ul>
+        {/* destructuramos los paramas de img */}
+				{images.map(({ id, title, url }) => (
+					<li key={id}>{title}</li>
+				))}
+			</ul>
 		</>
 	);
 };
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=U4P1GKXEB1Y38CeZLvTCRlNryZY9HC7I&q=${category}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=U4P1GKXEB1Y38CeZLvTCRlNryZY9HC7I&q=${category}&limit=20`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
