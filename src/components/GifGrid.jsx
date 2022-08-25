d59913a31d8353602d808eaf63012bcf68462ade
Francisco D <drz.y2k@gmail.com>
2022-08-25 17:40:57 -0500

    87. Mostrar mensaje de carga
    hacemos el muestreo del mensaje de cargando con un ternario que no es ternario, es un and logico, si se cumplle la primera condicion osea que isloading sea true, se ejecuta lo segundo ene ste caso lo segundo es un h2 con el texto cargando
    esto se va a mostrar cuando las imagenes aun no carguen

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -5,21 +5,26 @@ import { GifItem } from './GifItem';
 //se recomienda para llevar orden primero hacer las importaciones de react, luego las de terceros y luego las nuestras
 
 export const GifGrid = ({ category }) => {
-
-  //destructuramos los campos que enviamos desde el hook
-  const {images,isLoading}=useFetchGifs(category);
-
+	//destructuramos los campos que enviamos desde el hook
+	const { images, isLoading } = useFetchGifs(category);
 
 	return (
 		<>
 			<h3>{category}</h3>
+			{/* el null no se renderiza */}
+			{/* si isloading es true ejecuta lo que está despues, */}
+			{/* es un and logico */}
+			{/* funciona como si fuera un operador ternario pero solo con un if  */}
+			{isLoading && <h2>Cargando...</h2>}
 			<div className="card-grid">
 				{/* destructuramos los paramas de img */}
 				{images.map((image) => (
-					<GifItem key={image.id}
-          // de este modo le mandamos todos los parametros de image
-          //y podemos destrucutrar para usar en el hijo solo los que queramos
-          {...image} />
+					<GifItem
+						key={image.id}
+						// de este modo le mandamos todos los parametros de image
+						//y podemos destrucutrar para usar en el hijo solo los que queramos
+						{...image}
+					/>
 				))}
 			</div>
 		</>
